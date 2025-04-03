60b2ab953c07b264dd9545ca28e66f795ac16557
korba <kordiand@student.agh.edu.pl>
2025-04-04 00:22:11 +0200

    modyfikacja klasy

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from collections import defaultdict
 
 class Task:
-    def __init__(self, name, duration, prev, next, early_start, early_finish, rating):
+    def __init__(self, name, duration, prev, nxt):
         self.name = name
         self.duration = duration
-        self.prev = prev
-        self.next = next
-        self.early_start = early_start
-        self.early_finish = early_finish
-        self.rating = rating
+        self.prev = []
+        self.nxt = []
+        self.early_start = 0
+        self.early_finish = 0
+        self.late_start = float('inf')
+        self.late_finish = float('inf')
+        self.rating = 0
+
+
+
+
+
 
